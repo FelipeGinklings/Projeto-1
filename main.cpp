07e20810b7d02f0b6ba999bb1548dc4752685716
Felipe Ginklings <felipe.ginklings@gmail.com>
2024-03-01 04:03:03 -0300

    All files

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     int a, b, c, some;
+
+//     cout << "Please enter a number:\n";
+//     cin >> a >> b >> c;
+
+//     some = a + b + c;
+
+//     cout << "The result is: " << some;
+
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+
+int main() {
+  int some = 0;
+  int add;
+
+  for (int i = 0; i < 3; i++) {
+    cout << "Please enter a number: ";
+    cin >> add;
+
+    some += add;
+  }
+
+  cout << "The result is: " << some;
+
+  return 0;
+}
